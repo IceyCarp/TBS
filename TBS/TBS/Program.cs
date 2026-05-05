e32e52487b07d3b68277f49ddf69eba149e7c8c5
Darwin-our-lord <David@prebensen.dk>
2026-05-05 10:47:53 +0200

    version!

--- TBS/TBS/Program.cs
+++ TBS/TBS/Program.cs
@@ -7,7 +7,7 @@ namespace Game.Class
 {
     public class Program
     {
-        private const string CURRENT_VERSION = "1.0.33"; // Update this with each release
+        private const string CURRENT_VERSION = "1.0.34"; // Update this with each release
         private const string GITHUB_API_URL = "https://api.github.com/repos/EKDixen/TBS/releases/latest";
 
         static bool stopMultibleLoad = false;
